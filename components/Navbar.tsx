6f086aae55e844d4d474e137c57ea1dc80beed13
Harshal Margaj <harshal.margaj12@gmail.com>
2026-01-04 17:04:23 +0530

    design breadcrumb

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,14 @@ import { usePathname } from "next/navigation";
 const Navbar = () => {
 	const pathname = usePathname();
 
-	return <div className="p-5 border-b border-amber-100/10">{pathname}</div>;
+	return (
+		<div className="p-5 border-b border-amber-100/10">
+			<span className="border border-amber-100/10 p-2 px-4 rounded-md text-lg space-x-2 bg-neutral-800/60 backdrop-blur">
+				<span>/</span>
+				<span>{pathname.slice(1)}</span>
+			</span>
+		</div>
+	);
 };
 
 export default Navbar;
